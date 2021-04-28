48ff86af449fb5330b40a748fd93849ce0efb696
Rafik Rushanian <rafik@innorise.com>
2021-04-28 11:04:52 +0400

    Added another route

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,3 +5,7 @@ $router = app()->router;
 $router->get("module/check", ['middleware' => 'auth', function (\Illuminate\Http\Request $request){
     dd("Hello from package",$request);
 }]);
+
+$router->get("module/check2", ['middleware' => 'auth', function (\Illuminate\Http\Request $request){
+    dd("This is another check from same place");
+}]);
