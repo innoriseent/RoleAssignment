5c30cce5e5221e434f930ef6de1ae7286751a15b
Rafik Rushanian <rafik@innorise.com>
2021-05-03 15:13:31 +0400

    Added migrations and modules

---
+++ src/Models/Account.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Codeex\RoleAssignment;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    //
+}
---
+++ src/Models/Msp.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Codeex\RoleAssignment;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Msp extends Model
+{
+    //
+}
